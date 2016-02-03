b4d3a0ed638614fb5e6595a806c718ff316309ca
Shehabeldin Abdelgawad <shehabeldin.abdelgawad@de.bosch.com>
2016-02-03 12:48:25 +0100

    renamed input, code now uses vectors

--- src/robot_model_1.py
+++ src/robot_model_1.py
@@ -4,6 +4,9 @@ from casadi import *
 
 class SystemModel:
     def __init__(self):
+        """
+        for the parameter vector make sure that the inputs go in before the parameters(constants/time varying)
+        """
         self.p_num = []
         self.p_sym = []
         self.inputs = []
@@ -58,38 +61,35 @@ class RobotModel2D(SystemModel):
         # super(SystemModel, self).__init__(self)
         # numeric vars
         # numeric params
-        self.w_d_n = w_d_n
-        self.w_r_n = w_r_n
-        self.p_num = np.array([self.w_d_n, self.w_r_n])
+        w_d_n = w_d_n
+        w_r_n = w_r_n
+        self.p_num = np.array([w_d_n, w_r_n])
 
         # symbolic vars
 
         # symbolic params
-        self.w_d = SX.sym("w_d")
-        self.w_r = SX.sym("w_r")
-        self.p_sym = np.array([self.w_d, self.w_r])
-        self.t_sym = SX.sym("t")
+        w_d = SX.sym("w_d")
+        w_r = SX.sym("w_r")
+        self.p_sym = vertcat([w_d, w_r])
 
         # symbolic states
-        self.x1 = SX.sym("x1")  # x coordinate
-        self.x2 = SX.sym("x2")  # y coordinate
-        self.x3 = SX.sym("x3")  # theta (orientation)
-        self.states = np.array([self.x1, self.x2, self.x3])
+        self.state = SX.sym("x", 3)  # x coordinate, y coordinate, theta (orientation)
+        x = self.state  # used for short hand notation during the dev of equations
 
         # symbolic inputs
-        self.v_l = SX.sym("v_l")
-        self.v_r = SX.sym("v_r")
-        self.inputs = np.array([self.v_l, self.v_r])
+        v_l = SX.sym("v_l")
+        v_r = SX.sym("v_r")
+        self.inputs = vertcat([v_l, v_r])
 
         # outputs
-        self.outputs = self.states
+        self.outputs = self.state
 
         # casadi function
-        x1_dot = cos(self.x3)*(self.v_l+self.v_r)*0.5
-        x2_dot = sin(self.x3)*(self.v_l+self.v_r)*0.5
-        x3_dot = (self.v_r - self.v_l)/self.w_d
-        self.X_dot = np.array([x1_dot, x2_dot, x3_dot])
-        self.casadi_function = SXFunction('f', daeIn(x=self.states, p=np.append(self.p_sym, self.inputs), t=self.t_sym),
+        x1_dot = cos(x[2])*(v_l+v_r)*0.5
+        x2_dot = sin(x[2])*(v_l+v_r)*0.5
+        x3_dot = (v_r - v_l)/w_d
+        self.X_dot = vertcat([x1_dot, x2_dot, x3_dot])
+        self.casadi_function = SXFunction('f', daeIn(x=self.state, p=vertcat([self.inputs, self.p_sym]), t=self.t_sym),
                                           daeOut(ode=self.X_dot))
 
     def system_ode_odeint(self, x, t, params):
@@ -97,7 +97,7 @@ class RobotModel2D(SystemModel):
         v_l, v_r = params
         x1_dot = cos(x3)*(v_l+v_r)*0.5
         x2_dot = sin(x3)*(v_l+v_r)*0.5
-        x3_dot = (v_r - v_l)/self.w_d_n
+        x3_dot = (v_r - v_l)/self.p_num[0]
         X_dot = [x1_dot, x2_dot, x3_dot]
         return X_dot
 
@@ -108,36 +108,35 @@ class RobotModel3D(RobotModel2D):
         # super(RobotModel2D, self).__init__(self, w_d_n, w_r_n)
         RobotModel2D.__init__(self, w_d_n, w_r_n)
 
-        # numeric params and symbolic params are the same as RobotModle2D
+        # numeric params and symbolic params are the same as RobotModel2D
 
         # symbolic inputs # gyro readings
         roll_dot = SX.sym("dRoll")
         pitch_dot = SX.sym("dPitch")
         yaw_dot = SX.sym("dYaw")
-        self.inputs = np.append(self.inputs, np.array([roll_dot, pitch_dot, yaw_dot]))
+        additional_inputs = vertcat([roll_dot, pitch_dot, yaw_dot])
+        self.inputs = vertcat([self.inputs, additional_inputs])
 
         # symbolic states
-        # note that x3 is now the z coordinate
-        self.x4 = SX.sym("x4")  # roll
-        self.x5 = SX.sym("x5")  # pitch
-        self.x6 = SX.sym("x6")  # yaw
-        self.states = np.append(self.states, np.array([self.x4, self.x5, self.x6]))
+        self.state = SX.sym("x", 6)  # x[0] coordinate, y[1] coordinate, z[2] coordinate, roll[3], pitch[4], yaw[5]
+        x = self.state  # used for short hand notation during the dev of equations
 
         # outputs
-        self.outputs = self.states
+        self.outputs = self.state
 
         # casadi function
-        x1_dot = 0.5*(self.v_l+self.v_r)*cos(self.x6)*cos(self.x5)
-        x2_dot = 0.5*(self.v_l+self.v_r)*sin(self.x6)*cos(self.x5)
+        x1_dot = 0.5*(self.inputs[0]+self.inputs[1])*cos(x[5])*cos(x[4])
+        x2_dot = 0.5*(self.inputs[0]+self.inputs[1])*sin(x[5])*cos(x[4])
         # note the negative sign for pitch due to the RH coordinate
         # system with x forward y left and z up (ROS convention)
-        x3_dot = 0.5*(self.v_l+self.v_r)*sin(-self.x5)
+        x3_dot = 0.5*(self.inputs[0]+self.inputs[1])*sin(-x[4])
         x4_dot = roll_dot
         x5_dot = pitch_dot
-        x6_dot = (self.v_r - self.v_l)*cos(self.x4)/self.w_d
-        self.X_dot = np.array([x1_dot, x2_dot, x3_dot, x4_dot, x5_dot, x6_dot])
-        self.casadi_function = SXFunction("casadi_ode_function", daeIn(x=self.states,
-                                                                       p=np.append(self.p_sym, self.inputs),
+        x6_dot = (self.inputs[1] - self.inputs[0])*cos(x[3])/self.p_sym[0]
+        # x6_dot = yaw_dot  #more accurate
+        self.X_dot = vertcat([x1_dot, x2_dot, x3_dot, x4_dot, x5_dot, x6_dot])
+        self.casadi_function = SXFunction("casadi_ode_function", daeIn(x=self.state,
+                                                                       p=vertcat([self.inputs, self.p_sym]),
                                                                        t=self.t_sym), daeOut(ode=self.X_dot))
 
     def system_ode_odeint(self, x, t, params):
@@ -149,6 +148,25 @@ class RobotModel3D(RobotModel2D):
         x3_dot = 0.5*(v_l+v_r)*sin(x5)
         x4_dot = dRoll
         x5_dot = dPitch
-        x6_dot = (v_r - v_l)*cos(x4)/self.w_d_n
+        x6_dot = (v_r - v_l)*cos(x4)/self.p_num[0]
         X_dot = [x1_dot, x2_dot, x3_dot, x4_dot, x5_dot, x6_dot]
         return X_dot
+
+        # concatenation snippets
+        # print "vertcat([]) gives: %s type %s size %s" % (vertcat([self.p_sym, self.inputs]),
+        #                                                  type(vertcat([self.p_sym, self.inputs])),
+        #                                                  vertcat([self.p_sym, self.inputs]).shape)  # single vector
+        # print "vertcat(()) gives: %s type %s size %s" % (vertcat((self.p_sym, self.inputs)),
+        #                                                  type(vertcat((self.p_sym, self.inputs))),
+        #                                                  vertcat((self.p_sym, self.inputs)).shape)  # single vectror
+        # print "horzcat(()) gives: %s type %s size %s" % (horzcat([self.p_sym, self.inputs]),
+        #                                                  type(horzcat([self.p_sym, self.inputs])),
+        #                                                  horzcat([self.p_sym, self.inputs]).shape)  # gives a matrix
+        # print "np. array() gives: %s type %s size %s" % (np.array((self.p_sym, self.inputs)),
+        #                                                  type(np.array((self.p_sym, self.inputs))),
+        #                                                  np.array((self.p_sym, self.inputs)).shape)  # array of 2 vec
+        # np_1 = np.array([w_d, w_r])
+        # np_2 = np.array([v_l, v_r])
+        # print "np.append(np.array, np.array) gives: %s type %s size %s" % (np.append(np_1, np_2),
+        #                                                                    type(np.append(np_1, np_2)),
+        #                                                                    np.append(np_1, np_2).shape)  # vector
