94eb652d015015ae383470f1b0ac1146c9085eee
Shehabeldin Abdelgawad <shehabeldin.abdelgawad@de.bosch.com>
2016-02-10 14:28:35 +0100

    added checks, functions responsible for calculating linearized system, added default values for Q, R, P0 and included the setting of X0

--- src/Extended_Kalman_Filter.py
+++ src/Extended_Kalman_Filter.py
@@ -8,34 +8,54 @@ class KF:
     Base class for the Kalman filters containing the class variables which will be inherited by all other
     KF implementations
     """
-    def __init__(self, input_system):
+    # TODO: check if there is an advantage to using Dmatrix instead of np.eye
+    def __init__(self, input_system, X0=None, Qin=None, Rin=None, P0=None):
         self.system = input_system
-        # system (numeric)
-        self.A = None
-        self.B = None
-        self.C = None
-        self.L = None
-        self.Q = None  # sys cov
-        self.R = None  # measurement cov
 
-        # additional
-        self.A_t = None   # A transpose to reduce computational costs
-        self.L_t = None   # A transpose to reduce computational costs
-        self.X = None
         # system dimensions
         self.ns = self.system.state[0]  # number of states
         self.no = self.system.outputs[0]  # number of outputs
         self.ni = self.system.inputs[0]  # number of inputs
 
+        # system (numeric)
+        self.A = None
+        self.B = None
+        self.C = None
+        self.L = None
+        self.t = 0.0
+        if Qin is None:
+            print "WARNING: The Kalman filter's system covariance has not been initialized, system will use I"
+            self.Q = np.eye(self.ns)
+        else:
+            self.Q = Qin  # sys cov
+        if Rin is None:
+            print "WARNING: The Kalman filter's measurment covariance has not been initialized, system will use I"
+            self.R = np.eye(self.no)
+        else:
+            self.R = Rin  # measurement cov
+
+        # additional
+        self.A_t = None   # A transpose to reduce computational costs
+        self.L_t = None   # A transpose to reduce computational costs
+        self.X = None
+
         # outputs
-        self.X_k_1_c = None  # the most recent correction value
-        self.X_k_1_p = None  # this will be the most updated estimate (after correction or duing prediction) correction
+        if X0 is None:
+            print "WARNING: The Kalman filter's initial state has not been initialized"
+        self.X_k_1_c = X0  # the most recent correction value
+        self.X_k_1_p = X0  # this will be the most updated estimate (after correction or duing prediction) correction
         # will over write this value to keep it updated for the next prediction
-        self.P_k_1_c = None  # the most recent correction value (only values of corrected covariances)
-        self.P_k_1_p = None  # this will be the most updated estimate (after correction or duing prediction) correction
-        # will over write this value to keep it updated for the next prediction
+        if P0 is None:
+            print "WARNING: The Kalman filter's initial covariance has not been initialized, system will use I"
+            self.P_k_1_c = np.eye(self.ns)  # the most recent correction value (only values of corrected covariances)
+            self.P_k_1_p = np.eye(self.ns)  # this will be the most updated estimate (after correction or duing prediction)
+            # correction will over write this value to keep it updated for the next prediction
+        else:
+            self.P_k_1_c = P0  # the most recent correction value (only values of corrected covariances)
+            self.P_k_1_p = P0  # this will be the most updated estimate (after correction or duing prediction)
+            # correction will over write this value to keep it updated for the next prediction
         self.K = None  # the Kalman filter Gain
-        self.I = None  # implement an Identity matrix here instead of creating a new one every iteration
+        self.I = np.eye(self.ns)  # implement an Identity matrix here instead of creating a new one every iteration
 
     def mul_3(self, m1, m2, m3):
         # TODO: create unit test for this function
@@ -66,14 +86,8 @@ class DiscreteKF(KF):
     # TODO: add some constant calculations during the initialization of the
     # class to save repetitive calculations of static matrices
     # TODO: add unit-tests for this class
-    def __init__(self, input_system, initial_estimate_cov=None):
-        KF.__init__(self, input_system)
-        self.init_system(initial_estimate_cov)
-
-    def init_system(self, initial_estimate_cov):
-
-        if initial_estimate_cov is None:
-            self.P_k_1_c = 0  # needs to be changed
+    def __init__(self, input_system, X0=None, Qin=None, Rin=None, P0=None):
+        KF.__init__(self, input_system, X0=None,Qin=None, Rin=None, P0=None)
 
     def predict(self, system_input):
         # all in hat,x_hat (expected)
@@ -133,8 +147,8 @@ class HybridEKF(KF):
         I_options["integrator"] = "cvodes"
         csim = ControlSimulator("f", X_dot_function, I_options)
     """
-    def __init__(self, input_system):
-        KF.__init__(self, input_system)
+    def __init__(self, input_system, X0=None, Qin=None, Rin=None, P0=None):
+        KF.__init__(self, input_system, X0=None, Qin=None, Rin=None, P0=None)
 
         self.integ_ts = 0.2  # time step used for integration
         self.solver_integ = "cvodes"
@@ -142,7 +156,26 @@ class HybridEKF(KF):
         # symbolic variables
         self.P_sym = SX.sym("p", self.ns, self.ns)  # nxn symbolic matrix
         self.t_sym = SX.sym("t")  # symbolic time
-        self.A_sym = None
+        self.A_sym = SX.sym("a", self.ns, self.ns)
+        # not being used yet
+        self.L_sym = None
+        self.M_sym = None
+        self.C_sym = None
+
+        # functions used for linearization
+        if self.system.jac_X_dot_wrt_x is None:  # the jacobian has not been computed for this system yet
+            self.system.compute_jac_Xdot()
+        self.A_func = self.system.jac_X_dot_wrt_x
+        # not being used yet
+        self.L_func = None
+
+        if self.system.output_output_SX is None:  # no output equations specified assume Identity
+            self.C = np.eye(self.ni)  # HACK
+        else:
+            self.C_func = self.system.jac_output_wrt_x
+            # not being used yet
+            self.M_func = None
+
 
         # functions
         self.cov_estimate_func = None
@@ -152,7 +185,7 @@ class HybridEKF(KF):
         self.estimate_cov_integrator = None
 
     def create_p_dot_function(self):
-        P_dot = mul(self.A_sym, self.P_k_1_p) + mul(self.P_k_1_p, self.A_sym.T) + self.Q # TODO: generalize to L * Q * L.T
+        P_dot = mul(self.A_sym, self.P_k_1_p) + mul(self.P_k_1_p, self.A_sym.T) + self.Q # TODO: generalize to L * Q * L.T and make it a parameter in the eq below
         f_in = daeIn(x=self.P_sym, p=self.A_sym, t=self.t_sym)  # Q is considered a constant
         f_out = daeOut(ode=P_dot)
         self.cov_estimate_func = SXFunction("estimate_cov_function", f_in, f_out)
@@ -169,13 +202,15 @@ class HybridEKF(KF):
                                                   self.cov_estimate_func, sys_integ_opt)
 
     def prediction(self, system_input):
-        self.update_prediction_matrices()  # the A matrix
+        self.update_prediction_matrices(system_input)  # the A matrix
         # TODO: note the Q matrix is not Q tilde yet assuming that L is I
         # integrate the covariance
         self.estimate_cov_integrator.setInput(self.P_k_1_p, "x0")
         self.estimate_cov_integrator.setInput(self.A, "p")
         self.estimate_cov_integrator.evaluate()
         print self.estimate_cov_integrator.getOutput()
+        print "the type of the cov output is %s, the output is %s" %\
+        (type(self.estimate_cov_integrator.getOutput()[-1]), self.estimate_cov_integrator.getOutput()[-1])
 
         # TODO: add the var update
 
@@ -184,6 +219,8 @@ class HybridEKF(KF):
         self.system_integrator.setInput(vertcat(system_input, self.system.p_num), "p")
         self.system_integrator.evaluate()
         print self.system_integrator.getOutput()
+        print "the type of the system output is %s, the output is %s" %\
+        (type(self.system_integrator.getOutput()[-1]), self.system_integrator.getOutput()[-1])
         # TODO: add the var update
 
     def correction(self, system_output):
@@ -203,7 +240,7 @@ class HybridEKF(KF):
         # covariance correction
         # P_k_c = (I - K_k * C_k) * P_k_1 * (I - K_k * C_k).T + K_k * R_k * K_k.T
         M_1 = (self.I - mul(K_c, self.C))  # var to avoid multiple matrix multiplications
-        P_k_c = self.mul(M_1, self.P_k_l_p, M_1.T) + self.mul_3(K_c, self.R, K_c.T)  # covariance correction
+        P_k_c = self.mul_3(M_1, self.P_k_1_p, M_1.T) + self.mul_3(K_c, self.R, K_c.T)  # covariance correction
 
         self.P_k_1_c = P_k_c
         self.P_k_1_p = P_k_c
@@ -211,9 +248,12 @@ class HybridEKF(KF):
         self.X_k_1_p = X_k_c
         self.K = K_c
 
-    def update_prediction_matrices(self):
+    def update_prediction_matrices(self, system_input):
         # the A matrix must be evaluated and the L matrix the L matrix for later
-        pass
+        A_matrix_linearized = self.A_func({"x": self.X_k_1_p,
+                                           "p": vertcat([system_input, self.system.p_num]),
+                                           "t": self.t})
+        self.A = A_matrix_linearized
 
     def update_correction_matrices(self):
         # the C matrix amd the M matrix the M matrix for later
