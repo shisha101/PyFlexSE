f02155ec110f720c4cde7d8335b1369f00360feb
Shehabeldin Abdelgawad <shehabeldin.abdelgawad@de.bosch.com>
2016-02-12 15:06:03 +0100

    added cov plots

--- src/nonlinear_pendulum.py
+++ src/nonlinear_pendulum.py
@@ -48,6 +48,15 @@ class PendulumSystem(SystemModel):
                                      daeOut(ode=self.X_dot))
 
 
+def extract_cov_values(list_of_cov_matrices):
+    if list_of_cov_matrices is not None and len(list_of_cov_matrices) > 0:
+        list_of_cov = []
+        for sub_mat in list_of_cov_matrices:
+            diag_elements = np.diag(sub_mat).T
+            list_of_cov.append(diag_elements)
+    return np.vstack(list_of_cov)
+
+
 def plot_2d(x, y, fig_nr):
     fig = plt.figure(fig_nr, figsize=(8, 8))
 
@@ -80,100 +89,114 @@ def plot_2d(x, y, fig_nr):
 def plot_sim_vs_estim(x, y1, y2, fig_nr, y3=None):
     fig = plt.figure(fig_nr, figsize=(8, 8))
 
-    plot_1 = fig.add_subplot(211)
+    if y3 is None:
+        plot_number = 211
+    else:
+        plot_number = 411
+
+    plot_1 = fig.add_subplot(plot_number)
     plot_1.plot(x, y1[:, 0], "b-")
     plot_1.plot(x, y2[:, 0], "r--")
     plot_1.set_xlabel("time (s)")
     plot_1.set_ylabel("position x (m)")
+    plot_number += 1
 
-    plot_2 = fig.add_subplot(212)
+    plot_2 = fig.add_subplot(plot_number)
     plot_2.plot(x, y1[:, 1], "b-")
     plot_2.plot(x, y2[:, 1], "r--")
     plot_2.set_xlabel("time (s)")
     plot_2.set_ylabel("position y (m)")
-
-    # plot_3 = fig.add_subplot(413)
-    # mod_list_theta = map(lambda x: (x%(2*np.pi))*180/np.pi, y[:, 2])
-    # plot_3.plot(x, mod_list_theta)
-    # plot_3.set_xlabel("time (s)")
-    # plot_3.set_ylabel("theta (deg)")
-    # # plot_3.set_title("hi")
-    #
-    # plot_4 = fig.add_subplot(414)
-    # plot_4.plot(y[:, 0], y[:, 1])
-    # plot_4.set_xlabel("position x (m)")
-    # plot_4.set_ylabel("position y (m)")
+    plot_number += 1
+    
+    if y3 is not None:
+        plot_3 = fig.add_subplot(plot_number)
+        plot_3.plot(x, y3[:, 0], "r")
+        plot_3.set_xlabel("time (s)")
+        plot_3.set_ylabel("cov theta")
+        plot_number += 1
+    
+        plot_4 = fig.add_subplot(plot_number)
+        plot_4.plot(x, y3[:, 1], "r")
+        plot_4.set_xlabel("time (s)")
+        plot_4.set_ylabel("cov Omega")
+        plot_number += 1
 
     fig.tight_layout()
     fig.show()
 
 
 def main():
+    # model params
     mass = 1.0
     length = 0.5
     damping_factor = 0.05
 
+    # simulation params
     t_start = 0.0
     delta_t = 0.05
     t_end = 5.0
+    t_steps_between_measurements = 20  # how frequently do we get a measurement
+    start_state = np.array([[-20.0/180.0*np.pi], [0.0]])  # -20 degrees, zero omega
 
+    # EKF params
     model_trust_factor = 0.05  # how much do we trust our model compared to our measurements affects Q and R matrices
     initial_state_trust_factor = 0.001  # how much we trust our initial estimate P0
     initial_state_pre_multiplication_factor = -1.0  # affects the starting state of the estimator X0
-    t_steps_between_measurements = 20  # how frequently do we get a measurement
 
-    t = np.arange(t_start, t_end, delta_t)  # the end+delta_t is becasue the sim saves the initial sate
-    start_state = np.array([[-20.0/180.0*np.pi], [0.0]])  # -20 degrees, zero omega
+    t = np.arange(t_start, t_end, delta_t)  # the end+delta_t is because the sim saves the initial sate
+
+    # system
     pendulum = PendulumSystem(mass, length, damping_factor)
+
+    # system integrator
     opt = {"t0": t_start, "tf": delta_t}
     # non time varying system constant delta t with t0 = 0
     system_integrator = Integrator("real_pendulum_integrator", "cvodes", pendulum.X_dot_func, opt)
-    print start_state.shape
+
+    # Hybrid EKF
     hybrid_EKF = HybridEKF(pendulum, 0.05,
                            start_state * initial_state_pre_multiplication_factor,
                            Rin=np.eye(2),
                            Qin=(1 / model_trust_factor) * np.eye(2),
                            P0=(1 / initial_state_trust_factor) * np.eye(2))
+    # storage lists
     estimated_states = []
     estimated_cov = []
     real_states = []
-    current_state = deepcopy(start_state)
-    estimated_states.append(start_state * initial_state_pre_multiplication_factor)
-    estimated_cov.append(np.eye(2))
+    estimated_states.append(start_state * initial_state_pre_multiplication_factor)  # append state at t0
+    estimated_cov.append((1 / initial_state_trust_factor) * np.eye(2))  # append cov at t0
     real_states.append(start_state)
     print "the start state of the estimator is %s" % hybrid_EKF.X_k_1_p
     print "the start state of the system is %s" % start_state
+
     for index, time in enumerate(xrange(t.shape[0]-1)):
 
         # integrate the system
-        system_integrator.setInput(current_state, "x0")
+        system_integrator.setInput(real_states[-1], "x0")
         # system_integrator.setOption({"t0": 0.0, "tf": delta_t})
         system_integrator.setInput(np.append([0.0], pendulum.p_num), "p")
         system_integrator.evaluate()
         system_integ_output = system_integrator.getOutput().toArray()
-        current_state = system_integ_output
+        real_states.append(system_integ_output)
+
+        # do prediction or correction
         if index != 0 and index % t_steps_between_measurements == 0:
             hybrid_EKF.prediction(np.array([0.0]))
-            hybrid_EKF.correction(current_state)
+            hybrid_EKF.correction(real_states[-1])
         else:
             hybrid_EKF.prediction(np.array([0.0]))
-        print "***Start***"
-        print "EKF output is %s " % hybrid_EKF.X_k_1_p.toArray().T
-        print "step wise system integrator output is %s" % system_integ_output.T
-        print "***End***"
+        # print "***Start***"
+        # print "EKF output is %s " % hybrid_EKF.X_k_1_p.toArray().T
+        # print "step wise system integrator output is %s" % system_integ_output.T
+        # print "***End***"
         estimated_states.append(hybrid_EKF.X_k_1_p.toArray())
-        real_states.append(system_integ_output)
+
         estimated_cov.append(hybrid_EKF.P_k_1_p.toArray())
-    # print estimated_states
-    print estimated_cov[0]
-    reformated_out = np.hstack(estimated_states)
+
+    reformatted_out = np.hstack(estimated_states)
     real_states_ref = np.hstack(real_states)
-    print reformated_out.shape
-    print t.shape
-    print reformated_out.T[0:t.shape[0], :].shape
-    # print system_simulation[0, :]
-    # print reformated_out[:, 0]
-    plot_sim_vs_estim(t, real_states_ref.T, reformated_out.T, 1)
+    cov_values_vector = extract_cov_values(estimated_cov)
+    plot_sim_vs_estim(t, real_states_ref.T, reformatted_out.T, 1, cov_values_vector)
 
 if __name__ == '__main__':
     main()
